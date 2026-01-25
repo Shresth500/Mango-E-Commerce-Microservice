ba1a8607ffb393bd4c52397790a125d7011990bb
Shresth <shresth.chomal@ascendion.com>
2026-01-25 21:51:17 +0530

    Added Interface for Service Bus

---
+++ Mango.MessageBus/MessageBus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Mango.MessageBus;
+
+public class MessageBus : IMessageBus
+{
+    public Task PublishMessage(object message, string topic_queue_Name)
+    {
+        throw new NotImplementedException();
+    }
+}
