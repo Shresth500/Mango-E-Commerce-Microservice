d7a40634948f99215e96717f79abba4ecd0499c9
Shresth <shresth.chomal@ascendion.com>
2026-01-25 22:05:53 +0530

    Added the logic to push content inside the service bus

--- Mango.MessageBus/MessageBus.cs
+++ Mango.MessageBus/MessageBus.cs
@@ -1,10 +1,23 @@
-﻿
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System.Text;
+
+
 namespace Mango.MessageBus;
 
 public class MessageBus : IMessageBus
 {
-    public Task PublishMessage(object message, string topic_queue_Name)
+    public async Task PublishMessage(object message, string topic_queue_Name)
     {
-        throw new NotImplementedException();
+        var connectionString = ConnectionStrings.ServiceBusConnectionString;
+        await using var client = new ServiceBusClient(connectionString);
+        var sender = client.CreateSender(topic_queue_Name);
+        var jsonMessage = JsonConvert.SerializeObject(message);
+        var finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+        {
+            CorrelationId = Guid.NewGuid().ToString()
+        };
+        await sender.SendMessageAsync(finalMessage);
+        await client.DisposeAsync();
     }
 }
