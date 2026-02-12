efcea4f2b6f78a81519898c0ec2353019764082f
Shresth <shresth.chomal@ascendion.com>
2026-02-13 00:36:19 +0530

    Added the services to Microsoft azure

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -3,7 +3,10 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);
+if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
+    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
+else
+    builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
 builder.AddAppAuthetication();
